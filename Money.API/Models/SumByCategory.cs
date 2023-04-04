3a42152c6b225c14d98624eb48fcab919e5ab529
Marc UHART <marcuhart@gmail.com>
2023-04-04 16:02:25 -0700

    Group transactions by category and sub category

--- Money.API/Models/SumByCategory.cs
+++ Money.API/Models/SumByCategory.cs
@@ -5,5 +5,7 @@
         public string Name { get; set; }
 
         public decimal Amount { get; set; }
+
+        public IEnumerable<SumByCategory> SubCategory { get; set; }
     }
 }
--- Money.API/Services/DashboardService.cs
+++ Money.API/Services/DashboardService.cs
@@ -1,4 +1,5 @@
-﻿using Money.API.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Money.API.Data;
 using Money.API.Models;
 using System.Data;
 
@@ -16,17 +17,21 @@ namespace Money.API.Services
         public DashboardYear GetDashboard(int year)
         {
             var transactions = _context.Transactions
+                .Include(t => t.Category)
                 .Where(t => t.Date.Year == year)
                 .Select(t => new
                 {
                     t.CategoryId,
                     CategoryName = t.Category.Name,
+                    ParentCategoryId = t.Category.ParentId,
+                    ParentCategoryName = t.Category.Parent.Name,
                     t.Date.Month,
                     t.Date.Year,
                     Amount = t.Credit == 0 ? -t.Debit : t.Credit,
                     t.Debit,
                     t.Credit,
-                }).ToList();
+                })
+                .ToList();
 
              return transactions.GroupBy(d => d.Year)
                 .Select(d => new DashboardYear
@@ -38,11 +43,18 @@ namespace Money.API.Services
                                   Month = m.Key,
                                   Debit = m.Sum(d => -d.Debit),
                                   Credit = m.Sum(d => d.Credit),
-                                  Categories = m.GroupBy(c => new { c.CategoryId, c.CategoryName })
+                                  Categories = m.GroupBy(c => new { c.ParentCategoryId, c.ParentCategoryName })
                                                 .Select(c => new SumByCategory
                                                 {
-                                                    Name = c.Key.CategoryName,
+                                                    Name = c.Key.ParentCategoryName,
                                                     Amount = c.Sum(u => u.Amount),
+                                                    SubCategory = c.GroupBy(z => new { z.CategoryId, z.CategoryName })
+                                                                    .Select(c => new SumByCategory
+                                                                    {
+                                                                        Name = c.Key.CategoryName,
+                                                                        Amount = c.Sum(u => u.Amount),
+                                                                        SubCategory = null
+                                                                    })
                                                 })
                                                 .OrderBy(o => o.Amount)
                               })
