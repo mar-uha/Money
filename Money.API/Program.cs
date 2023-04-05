b0f03b933cb47e7623f06ae15878720f4370f95b
Marc UHART <marcuhart@gmail.com>
2023-04-05 10:08:20 -0700

    Adding swagger

--- Money.API/Program.cs
+++ Money.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Money.API.Data;
 using Money.API.Services;
 
@@ -15,9 +16,41 @@ builder.Services.AddScoped<ICategoryService, CategoryService>()
     .AddScoped<ITransactionService, TransactionService>()
     .AddScoped<IDashboardService, DashboardService>();
 
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Money API",
+        Description = "An ASP.NET Core Web API for managing personnal bank account",
+        //TermsOfService = new Uri("https://example.com/terms"),
+        //Contact = new OpenApiContact
+        //{
+        //    Name = "Example Contact",
+        //    Url = new Uri("https://example.com/contact")
+        //},
+        //License = new OpenApiLicense
+        //{
+        //    Name = "Example License",
+        //    Url = new Uri("https://example.com/license")
+        //}
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 app.UseHttpsRedirection();
 
